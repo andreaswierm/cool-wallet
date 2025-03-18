ab6325e2842cc9ba791eb456bc705177bea66164
Andreas Wiermann Casas <andreas.w.dev@gmail.com>
2025-03-18 10:13:57 -0300

    config wallet

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,15 +1,16 @@
 export const config = {
   // Wallet name will be seen as the Wallet name
-  walletName: "<wallet-name>",
+  walletName: "Cool wallet From Dynamic",
   // Wallet icon will be seen as the Wallet icon
-  walletIcon: "<wallet-icon>",
+  walletIcon:
+    "https://images.vexels.com/media/users/3/239492/isolated/preview/ae36a88728be8e1f8b3596007d1123b0-doodle-da-cor-do-planeta.png",
   // URL of your wallet domain (e.g. https://dynamic.example.com)
-  walletUrl: "<wallet-url>",
+  walletUrl: "https://dynamic.andreaswiermann.com",
   // Environment ID of your wallet (e.g. 1234567890)
-  environmentId: "<environment-id>",
+  environmentId: "d6b50483-6b54-4df9-979b-6d19bc1fe159",
   // EIP6963 configuration
   eip6963: {
     // RDNS of your wallet (e.g. com.example.wallet)
-    rdns: "<rdns>",
+    rdns: "cool.andreaswiermann.com",
   },
 };
